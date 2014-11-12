5b33c65e935658d6f424c3aa91ec486fc82cd4f6
jigou <jarrod.igou@gmail.com>
2014-11-11 20:39:38 -0600

    Added creation of interesting_images table

--- database_create.sql
+++ database_create.sql
@@ -1,22 +1,21 @@
-CREATE DATABASE hackathon_marsrovers;
-
+CREATE DATABASE IF NOT EXISTS hackathon_marsrovers;
 USE hackathon_marsrovers;
 
-CREATE TABLE lastSol (
+CREATE TABLE IF NOT EXISTS lastSol (
 	meraSol int not null,
 	merbSol int not null,
 	mslSol int not null
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE images (
+CREATE TABLE IF NOT EXISTS images (
 	id int not null auto_increment,
 	URL varchar(150), 
 	metadata longtext,
 	submissionCount int,
 	PRIMARY KEY (id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE userSubmissions (
+CREATE TABLE IF NOT EXISTS userSubmissions (
 	id int not null auto_increment,
 	imageId int not null,
 	FOREIGN KEY (imageId) 
@@ -27,7 +26,14 @@ CREATE TABLE userSubmissions (
 	plotpoints longtext,
 	userId varchar(150),
 	PRIMARY KEY (id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE USER 'roveruser'@'localhost' IDENTIFIED BY 'wqeindsa235';
-GRANT select,insert,update,delete on hackathon_marsrovers.* to roveruser@'localhost' identified by 'wqeindsa235';
+CREATE TABLE IF NOT EXISTS interesting_images (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  url varchar(255) DEFAULT NULL,
+  timestamp varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+GRANT select,insert,update,delete on hackathon_marsrovers.* to roveruser@'%' identified by 'wqeindsa235';
